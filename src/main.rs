f9c9a9382799d952410304134c596fe9b7cba35b
Edmund Whitehead <ed@Edmunds-MacBook-Pro.local>
2020-12-26 19:33:08 -0700

    implementing chapter 4

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,27 @@
 mod vec3;
 mod ray;
 
+fn hit_sphere(center: vec3::Vec3, radius: f32, r: ray::Ray) -> bool {
+    let oc = r.origin() - center;
+    let a = r.direction().dot(r.direction());
+    let b = 2.0 * oc.dot(r.direction());
+    let c = oc.dot(oc) - radius*radius;
+    let discriminant = b*b - 4.0*a*c;
+    discriminant > 0.0
+}
+
 fn color(r: ray::Ray) -> vec3::Vec3 {
+    if hit_sphere(vec3::Vec3{ e: [0.0, 0.0, -1.0]}, 0.5, r) {
+        return vec3::Vec3{ e: [1.0, 0.0, 0.0]};
+    }
     let unit_direction = vec3::unit_vector(r.direction());
     let t = 0.5 * (unit_direction.y() + 1.0);
-    (1.0 - t) * vec3::Vec3{ e: [1.0, 1.0, 1.0]} + t * vec3::Vec3{ e: [0.0, 0.0, 1.0]}
+    (1.0 - t) * vec3::Vec3{ e: [1.0, 1.0, 1.0]} + t * vec3::Vec3{ e: [0.0, 0.0, 0.0]}
 }
 
 fn main() {
-    let nx = 200;
-    let ny = 100;
+    let nx = 4000;
+    let ny = 2000;
 
     print!("P3\n{} {} \n255\n", nx, ny);
 
