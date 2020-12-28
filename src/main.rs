187040794ca6b534c9e2f288018e611fdc88b8e3
Edmund Whitehead <ed@Edmunds-MacBook-Pro.local>
2020-12-27 20:03:21 -0700

    implementing chapter 7

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use rand::Rng;
 
 mod vec3;
 mod ray;
@@ -6,16 +5,27 @@ mod hitable;
 mod sphere;
 mod camera;
 
+fn random_in_unit_sphere() -> vec3::Vec3 {
+    let mut p: vec3::Vec3;
+    loop {
+        p = 2.0 * vec3::Vec3{ e: [rand::random::<f32>(), rand::random::<f32>(),
+                                      rand::random::<f32>()]}
+                - vec3::Vec3{ e: [1.0, 1.0, 1.0] };
+        if !(p.squared_length() >= 1.0) { break; }
+    }
+    p
+}
+
 fn color(r: ray::Ray, world: &dyn hitable::Hitable) -> vec3::Vec3 {
-    let rec = world.hit(r, 0.0, f32::INFINITY);
-    if !rec.is_none() {
-        return 0.5 * vec3::Vec3{ e: [rec.unwrap().normal.x()+1.0,
-                                     rec.unwrap().normal.y()+1.0,
-                                     rec.unwrap().normal.z()+1.0] };
+    let wrapped_rec = world.hit(r, 0.001, f32::INFINITY);
+    if !wrapped_rec.is_none() {
+        let rec = wrapped_rec.unwrap();
+        let target = rec.p + rec.normal + random_in_unit_sphere();
+        return 0.5 * color(ray::Ray{ a: rec.p, b: target-rec.p }, world);
     } else {
         let unit_direction = vec3::unit_vector(r.direction());
         let t = 0.5 * (unit_direction.y() + 1.0);
-        return (1.0 - t) * vec3::Vec3{ e: [1.0, 1.0, 1.0]} + t * vec3::Vec3{ e: [0.0, 0.0, 0.0]}
+        return (1.0 - t) * vec3::Vec3{ e: [1.0, 1.0, 1.0]} + t * vec3::Vec3{ e: [0.2, 0.5, 0.7]}
     }
 }
 
@@ -38,9 +48,9 @@ fn main() {
         for i in 0..nx {
             let mut col = vec3::Vec3{ e: [0.0, 0.0, 0.0]};
             for s in 0..ns {
-                let u = (i as f32 + rand::thread_rng().gen_range(0.0..1.0))
+                let u = (i as f32 + rand::random::<f32>())
                         / nx as f32;
-                let v = (j as f32 + rand::thread_rng().gen_range(0.0..1.0))
+                let v = (j as f32 + rand::random::<f32>())
                         / ny as f32;
 
                 let r = cam.get_ray(u, v);
@@ -48,6 +58,7 @@ fn main() {
                 col += color(r, &world);
             }
             col /= ns as f32;
+            col = vec3::Vec3{ e: [col.r().sqrt(), col.g().sqrt(), col.b().sqrt()] };
             let icol = col * 255.99;
             println!("{} {} {}", icol.r() as i32, icol.g() as i32, icol.b() as i32);
         }
